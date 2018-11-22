6a29b9fda2e75fa785c8c3b31b65f659057c6ba4
Ronnie <ronnieduardo.mh@gmail.com>
2018-11-21 22:42:43 -0500

    hola

--- src/ac_test_git/Ac_test_git.java
+++ src/ac_test_git/Ac_test_git.java
@@ -16,7 +16,7 @@ public class Ac_test_git {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hola desde git");
+        System.out.println("hola desde git...");
     }
     
 }
